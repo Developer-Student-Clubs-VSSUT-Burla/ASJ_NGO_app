442657a3880aacdc77ef330d0d920e44d88a63e2
dhairya <joshidhairaya2002@gmail.com>
2021-12-30 23:27:20 +0530

    Added view model and live data

---
+++ app/src/main/java/com/pkpanda/ngofinder/NgoProfile_Dhairya.kt
@@ -0,0 +1,11 @@
+package com.pkpanda.ngofinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NgoProfile_Dhairya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ngo_profile_dhairya)
+    }
+}
