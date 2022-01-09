316893d21a60c4313488d72e844328493132492b
Ankit Mohanty <56730013+sovan580@users.noreply.github.com>
2022-01-10 04:43:38 +0530

    Donation toast added.

--- app/src/main/java/com/example/asjapp/DonateFragment.kt
+++ app/src/main/java/com/example/asjapp/DonateFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.asjapp.databinding.FragmentDonateBinding
 import com.example.asjapp.databinding.FragmentJoinBinding
@@ -24,6 +25,9 @@ class DonateFragment : Fragment() {
 
         binding.back.setOnClickListener {
             findNavController().navigate(R.id.action_donateFragment_to_ngo_Profile2)
+        }
+        binding.submit.setOnClickListener{
+            Toast.makeText(context, "Donation Successful", Toast.LENGTH_SHORT).show()
         }
 
         return view
