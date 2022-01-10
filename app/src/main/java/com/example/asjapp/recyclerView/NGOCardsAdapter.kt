db24d4204600b8b956a0dc1e18131cc38242e9fb
Adarsh <adarsh.goswami9502@gmail.com>
2022-01-10 11:26:40 +0530

    Respective Details Added

--- app/src/main/java/com/example/asjapp/recyclerView/NGOCardsAdapter.kt
+++ app/src/main/java/com/example/asjapp/recyclerView/NGOCardsAdapter.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.asjapp.R
 import com.example.asjapp.TabbedFragmentDirections
 
-class NGOCardsAdapter(private val name: Array<String>, private val detail: Array<String>) :
+class NGOCardsAdapter(private val name: Array<String>, private val detail: Array<String>, private val fullDetail: Array<String>) :
     RecyclerView.Adapter<NGOCardsAdapter.NGOViewHolder>() {
 
     class NGOViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -27,10 +27,11 @@ class NGOCardsAdapter(private val name: Array<String>, private val detail: Array
     override fun onBindViewHolder(holder: NGOViewHolder, position: Int) {
         val currentName = name[position]
         val currentDetails = detail[position]
+        val currentFullDetails = fullDetail[position]
         holder.nameORG.text = currentName
         holder.details.text = currentDetails
         holder.itemView.setOnClickListener {
-            val action = TabbedFragmentDirections.actionTabbedFragmentToNgoProfile(currentName,currentDetails)
+            val action = TabbedFragmentDirections.actionTabbedFragmentToNgoProfile(currentName,currentDetails,currentFullDetails)
             Navigation.createNavigateOnClickListener(action).onClick(holder.itemView)
         }
     }
